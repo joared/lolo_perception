Added feature model for pose simulation, added node for estimating average poses (used in pose simulation launch), fixed reprojection calculation bug in reprojection_utils, DSPose now calculates its standard deviation as rmseMax/4 according under assumptions that +- rmseMax covers 95% of the bell curve. Changed some names and other small changes

nodes/pose_average_estimator_node.py
@@ -11,11 +11,39 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 
-from lolo_perception.perception_utils import PoseAverageAndCovarianceEstimator
 from lolo_perception.perception_ros_utils import vectorToPose, poseToVector
 
+class PoseAverageAndCovarianceEstimator:
+    """
+    Utility class to use when evaluating uncertainty of pose and image points.
+    Make sure all the samples are correct (watch out for outliers)
+    """
+    def __init__(self, nSamples=100):
+        self.poseVecs = [] # list of pose vectors [tx, ty, tz, rx, ry, rz]
+        self.nSamples = nSamples
 
-class AveragePosesAndImagePoints:
+    def add(self, translationVec, rotationVec):
+
+        self.poseVecs.insert(0, list(translationVec) + list(rotationVec))
+        self.poseVecs = self.poseVecs[:self.nSamples]
+
+    def calcCovariance(self):
+        if len(self.poseVecs) > 1:
+            poseCov = np.cov(self.poseVecs, rowvar=False)
+        else:
+            poseCov = np.zeros((6,6), dtype=np.float32)
+
+        return poseCov
+
+    def calcAverage(self):
+        # This only works for small differences in rotation and 
+        # when rotations are not near +-pi
+        if self.poseVecs:
+            return np.mean(self.poseVecs, axis=0)
+
+        return np.zeros((6,), dtype=np.float32)
+
+class PoseAverageEstimatorNode:
     def __init__(self, nSamples, poseTopic):
         self.estimator = PoseAverageAndCovarianceEstimator(nSamples=nSamples)
         self.frameID = None # will be the same as the subscribed poses
@@ -35,7 +63,6 @@ class AveragePosesAndImagePoints:
     def calcPose(self):
         poseAvg = self.estimator.calcAverage()
         poseCov = self.estimator.calcCovariance()
-        
         return vectorToPose(self.frameID, poseAvg[:3], poseAvg[3:], poseCov)
 
     def run(self):
@@ -46,4 +73,7 @@ class AveragePosesAndImagePoints:
 
 
 if __name__ == '__main__':
-    pass
+    rospy.init_node("average_pose_estimator")
+    poseTopic = rospy.get_param("~pose_topic")
+    estimator = PoseAverageEstimatorNode(nSamples=100, poseTopic=poseTopic)
+    estimator.run()

nodes/pose_estimation_simulation.py
@@ -20,28 +20,28 @@ from lolo_perception.pose_estimation import DSPoseEstimator, calcPoseCovariance
 from lolo_perception.perception_utils import plotPosePoints, plotPoints, plotAxis, projectPoints, plotPosePointsWithReprojection
 from lolo_perception.perception_ros_utils import vectorToPose, vectorToTransform, featurePointsToMsg
 
-def __publishFeaturePoses(frameID, featurePoints, poseCovariance):
-    timeStamp = rospy.Time.now()
-    for i, point in enumerate(featurePoints):
-        frameID = "{}/{}".format(frameID, i+1)
-        covariance = np.zeros([0]*36) # TODO: use poseCovariance
-        p = vectorToPose(frameID, point, np.zeros((1, 3)), covariance)
 
 class PoseSimulation:
-    def __init__(self):
+    def __init__(self, camera, featureModel):
         self.transformPublisher = rospy.Publisher("/tf", tf.msg.tfMessage, queue_size=1)
         self.cvBridge = CvBridge()
         self.imagePublisher = rospy.Publisher("pose/image", Image, queue_size=1)
 
-        self.posePublisher = rospy.Publisher('light_true/pose', PoseWithCovarianceStamped, queue_size=1)
-        self.featurePosesPublisher = rospy.Publisher('light_true/poses', PoseArray, queue_size=1)
-        self.poseNoisedPublisher = rospy.Publisher('light_noised/pose', PoseWithCovarianceStamped, queue_size=1)
-        self.featurePosesNoisedPublisher = rospy.Publisher('light_noised/poses', PoseArray, queue_size=1)
+        self.truePoseTopic = 'docking_station_true/pose'
+        self.trueLightsTopic = 'docking_station_true/lights'
+        self.noisedPoseTopic = 'docking_station_noised/pose'
+        self.noisedLightsTopic = 'docking_station_noised/lights'
+        self.posePublisher = rospy.Publisher(self.truePoseTopic, PoseWithCovarianceStamped, queue_size=1)
+        self.featurePosesPublisher = rospy.Publisher(self.trueLightsTopic, PoseArray, queue_size=1)
+        self.poseNoisedPublisher = rospy.Publisher(self.noisedPoseTopic, PoseWithCovarianceStamped, queue_size=1)
+        self.featurePosesNoisedPublisher = rospy.Publisher(self.noisedLightsTopic, PoseArray, queue_size=1)
+
+        self.poseEstimator = DSPoseEstimator(camera, featureModel, ignorePitch=False, ignoreRoll=False)
 
     def _getKey(self):
         pass
 
-    def test2DNoiseError(self, camera, featureModel, sigmaX, sigmaY):
+    def test2DNoiseError(self, sigmaX, sigmaY):
         """
         camera - Camera object
         featureModel - FeatureModel object
@@ -53,14 +53,18 @@ class PoseSimulation:
         img = img.astype(np.uint8)
         cv.imshow("feature model modification", img)
 
-        poseEstimator = DSPoseEstimator(camera, featureModel, ignorePitch=False, ignoreRoll=False)
+        print("Feature model uncertainty:", featureModel.uncertainty)
+        featureModel.uncertainty = 0.008545
+        featureModel.uncertainty = 0.03
+        print("Feature model uncertainty:", featureModel.uncertainty)
+
         featurePoints = featureModel.features
         biasedFeaturePoints = featurePoints.copy()
-        biasedFeaturePoints[0][0] += 0.005
-        biasedFeaturePoints[0][1] += 0.005
-        biasedFeaturePoints[0][2] += 0.005
+        biasedFeaturePoints[0][0] += 0.0
+        biasedFeaturePoints[0][1] += 0.0
+        biasedFeaturePoints[0][2] += 0.0
 
-        trueTrans = np.array([0, 0, 1], dtype=np.float32)
+        trueTrans = np.array([0, 0, 6], dtype=np.float32)
         trueRotation = np.eye(3, dtype=np.float32)
         ax, ay, az = 0, 0, 0 # euler angles
         featureIdx = -1
@@ -107,11 +111,10 @@ class PoseSimulation:
                     featureIdx = -1
                     print(chr(key))
 
-
             r = R.from_euler("YXZ", (ay, ax, 0))
             trueRotation = r.as_rotvec().transpose()
 
-            projPoints = projectPoints(trueTrans, trueRotation, camera, featurePoints)
+            #projPoints = projectPoints(trueTrans, trueRotation, camera, featurePoints)
             biasedProjPoints = projectPoints(trueTrans, trueRotation, camera, biasedFeaturePoints)
 
             # Introduce noise:
@@ -126,30 +129,41 @@ class PoseSimulation:
             estTranslationVec = trueTrans.copy()
             estRotationVec = trueRotation.copy()
 
+            # TODO: calculate based on feature model uncertainty instead
             pixelCovariance = np.array([[sigmaX**2, 0], [0, sigmaY**2]])
 
             # TODO: use something else than max intensity (255)?
             lightSourcesNoised = [ LightSource( np.array([[[ p[0], p[1] ]]], dtype=np.int32), intensity=255 ) for p in projPointsNoised]
-            dsPoseNoised = poseEstimator.estimatePose(lightSourcesNoised, 
+            dsPoseNoised = self.poseEstimator.estimatePose(lightSourcesNoised, 
                                                       estTranslationVec,
                                                       estRotationVec)
-            dsPoseNoised.calcCovariance(pixelCovariance)
-
+            
+            # calculates covariance based on max reprojection rmse from feature uncertainty
+            dsPoseNoised.calcCovariance()
             trueCovariance = calcPoseCovariance(camera, featureModel, trueTrans, trueRotation, pixelCovariance)
+         
+            timeStamp = rospy.Time.now()
 
-            pArray = featurePointsToMsg("lights_noised", featurePoints)
-            self.featurePosesNoisedPublisher.publish(pArray)
-
-            pArray = featurePointsToMsg("lights_true", biasedFeaturePoints)
+            pArray = featurePointsToMsg(self.truePoseTopic, biasedFeaturePoints, timeStamp=timeStamp)
             self.featurePosesPublisher.publish(pArray)
 
-            tTrue = vectorToTransform("camera1", "lights_true", trueTrans, trueRotation)
-            tNoised = vectorToTransform("camera2", "lights_noised", dsPoseNoised.translationVector, dsPoseNoised.rotationVector)
+            pArray = featurePointsToMsg(self.noisedPoseTopic, featurePoints, timeStamp=timeStamp)
+            self.featurePosesNoisedPublisher.publish(pArray)
+
+            tTrue = vectorToTransform("camera1", 
+                                      self.truePoseTopic, 
+                                      trueTrans, 
+                                      trueRotation, 
+                                      timeStamp=timeStamp)
+            tNoised = vectorToTransform("camera2", 
+                                        self.noisedPoseTopic, 
+                                        dsPoseNoised.translationVector, 
+                                        dsPoseNoised.rotationVector, 
+                                        timeStamp=timeStamp)
             self.transformPublisher.publish(tf.msg.tfMessage([tTrue, tNoised]))
 
-            self.posePublisher.publish( vectorToPose("camera1", trueTrans, trueRotation, trueCovariance) )
-            self.poseNoisedPublisher.publish( vectorToPose("camera2", dsPoseNoised.translationVector, dsPoseNoised.rotationVector, dsPoseNoised.covariance) )
-
+            self.posePublisher.publish( vectorToPose("camera1", trueTrans, trueRotation, trueCovariance, timeStamp=timeStamp) )
+            self.poseNoisedPublisher.publish( vectorToPose("camera2", dsPoseNoised.translationVector, dsPoseNoised.rotationVector, dsPoseNoised.covariance, timeStamp=timeStamp) )
 
             imgTemp = img.copy()
             # true axis
@@ -185,4 +199,4 @@ if __name__ =="__main__":
     cameraYamlPath = os.path.join(rospkg.RosPack().get_path("lolo_perception"), "camera_calibration_data/{}".format(cameraYaml))
     camera = Camera.fromYaml(cameraYamlPath)
 
-    PoseSimulation().test2DNoiseError(camera, featureModel, sigmaX=.00001, sigmaY=.00001)
+    PoseSimulation(camera, featureModel).test2DNoiseError(sigmaX=2, sigmaY=2)

src/lolo_perception/perception_ros_utils.py
@@ -47,12 +47,9 @@ def vectorToTransform(frameID, childFrameID, translationVector, rotationVector, 
     return t
 
 def poseToVector(pose):
-    translationVector = np.array([0]*3)
-    rotationVector = np.array([0]*3)
-
-    translationVector[0] = pose.pose.pose.position.x
-    translationVector[1] = pose.pose.pose.position.y
-    translationVector[2] = pose.pose.pose.position.z
+    translationVector = np.array([pose.pose.pose.position.x, 
+                                  pose.pose.pose.position.y, 
+                                  pose.pose.pose.position.z])
 
     quat = [pose.pose.pose.orientation.x,
             pose.pose.pose.orientation.y,

src/lolo_perception/pose_estimation.py
@@ -13,6 +13,31 @@ from lolo_perception.feature_extraction import featureAssociation
 
 from lolo_perception.reprojection_utils import calcPoseReprojectionRMSEThreshold
 
+from numpy.linalg import lapack_lite
+#lapack_routine = lapack_lite.dgesv
+
+# Looking one step deeper, we see that solve performs many sanity checks.  
+# Stripping these, we have:
+def fastInverse(A):
+    """
+    https://stackoverflow.com/questions/11972102/is-there-a-way-to-efficiently-invert-an-array-of-matrices-with-numpy
+    """
+    b = np.identity(A.shape[2], dtype=A.dtype)
+
+    n_eq = A.shape[1]
+    n_rhs = A.shape[2]
+    pivots = np.zeros(n_eq, np.intc)
+    identity  = np.eye(n_eq)
+    def lapack_inverse(a):
+        b = np.copy(identity)
+        pivots = np.zeros(n_eq, np.intc)
+        results = lapack_lite.dgelsd(n_eq, n_rhs, a, n_eq, pivots, b, n_eq, 0)
+        if results['info'] > 0:
+            raise np.LinAlgError('Singular matrix')
+        return b
+
+    return np.array([lapack_inverse(a) for a in A])
+
 def calcPoseCovariance(camera, featureModel, translationVector, rotationVector, pixelCovariance):
     _, jacobian = cv.projectPoints(featureModel.features, 
                                    rotationVector.reshape((3, 1)), 
@@ -27,9 +52,13 @@ def calcPoseCovariance(camera, featureModel, translationVector, rotationVector, 
     # How to rotate covariance: https://robotics.stackexchange.com/questions/2556/how-to-rotate-covariance
 
     sigma = scipy.linalg.block_diag(*[pixelCovariance]*len(featureModel.features))
+
     sigmaInv = np.linalg.inv(sigma)
+    #sigmaInv = fastInverse(np.reshape(sigma, (sigma.shape[0], sigma.shape[1], 1))).reshape(sigma.shape)
     try:
-        covariance = np.linalg.inv(np.matmul(np.matmul(J.transpose(), sigmaInv), J))
+        mult = np.matmul(np.matmul(J.transpose(), sigmaInv), J)
+        covariance = np.linalg.inv(mult)
+        #covariance = fastInverse(np.reshape(mult, (mult.shape[0], mult.shape[1], 1))).reshape(mult.shape)
     except np.linalg.LinAlgError as e:
         print("Singular matrix")
 
@@ -127,16 +156,15 @@ class DSPose:
         # About covariance: https://manialabs.wordpress.com/2012/08/06/covariance-matrices-with-a-practical-example/
         # Article: https://www.sciencedirect.com/science/article/pii/S0029801818301367
         # Stack overflow: https://stackoverflow.com/questions/36618269/uncertainty-on-pose-estimate-when-minimizing-measurement-errors
+        # Standard deviation = reprojection rmse/4: https://www.thoughtco.com/range-rule-for-standard-deviation-3126231
         #jacobian - 2*nPoints * 14
         # jacobian - [rotation, translation, focal lengths, principal point, dist coeffs]
 
         # TODO: is RMSE a godd approximation of the standard deviation? (same formula?)
         if pixelCovariance is None:
-            # TODO: rmse or rmseMax?? I think rmseMax makes more sense 
-            #sigmaX = self.rmse
-            #sigmaY = self.rmse
-            sigmaX = self.rmseMax
-            sigmaY = self.rmseMax
+            # https://www.thoughtco.com/range-rule-for-standard-deviation-3126231
+            sigmaX = self.rmseMax/4
+            sigmaY = self.rmseMax/4
             pixelCovariance = np.array([[sigmaX**2, 0], [0, sigmaY**2]])
 
         covariance = calcPoseCovariance(self.camera, 

src/lolo_perception/reprojection_utils.py
@@ -15,8 +15,11 @@ def calcErrPoint(p, deltaE):
 
     # TODO: if pNorm >> deltaE, np.arcsin(...) can be neglected
     if px < 0:
-        beta = np.arctan2(-px, -pz)
+        #beta = np.arctan2(-px, -pz)
+        #theta = -beta + np.arcsin( deltaE/pNorm )
+        beta = np.arctan2(px, pz) + np.pi
         theta = -beta + np.arcsin( deltaE/pNorm )
+    
     else:
         beta = np.arctan2(px, pz)
         theta = -beta - np.arcsin( deltaE/pNorm )
@@ -27,22 +30,17 @@ def calcErrPoint(p, deltaE):
 
 def maxReprojectionError(point, f, deltaE):
     newPoint = calcErrPoint(point, deltaE)
-    reprojErr = f*point[0]/point[1]- f*newPoint[0]/newPoint[1]
+    reprojErr = f*point[0]/float(point[1])- f*newPoint[0]/float(newPoint[1])
     return reprojErr
 
-def doTheStuff(img, f, fScale, deltaE, center, points, calcErrMethod):
+def plot2DReprojection(img, f, fScale, deltaE, center, points, calcErrMethod):
     for point in points:
         deltaEVec = calcErrMethod(point, deltaE)
 
         newPoint = deltaEVec
         
-        rx1 = fScale*f*point[0]/point[1], fScale*f
-        rx2 = fScale*f*newPoint[0]/newPoint[1], fScale*f
-        #rx1Pixel = fPixel*point[0]/point[1]
-        #rx2Pixel = fPixel*newPoint[0]/newPoint[1]
-        #reprojErr = abs(rx1Pixel-rx2Pixel)
-        reprojErr = abs(f*point[0]/point[1]- f*newPoint[0]/newPoint[1])
-        #print("Reprojection error: {}".format(reprojErr))
+        rx1 = fScale*float(f)*point[0]/point[1], fScale*f
+        rx2 = fScale*float(f)*newPoint[0]/newPoint[1], fScale*f
 
         # detected point
         point = adjustToCenter(center, point)
@@ -51,14 +49,14 @@ def doTheStuff(img, f, fScale, deltaE, center, points, calcErrMethod):
         cv.line(img, center, point, color=(0,0,255/2))
 
         # reprojection point
-        
         rx1 = adjustToCenter(center, rx1)
         rx1 = toValidPixel(rx1)
         cv.line(img, (rx1[0], rx1[1]-5), (rx1[0], rx1[1]+5), color=(0,0,255))
 
         # worst true point
-        newPoint = (int(round(newPoint[0])), int(round(newPoint[1])))
+         #(int(round(newPoint[0])), int(round(newPoint[1])))
         newPoint = adjustToCenter(center, newPoint)
+        newPoint = toValidPixel(newPoint)
         cv.circle(img, newPoint, 1, (0,255,0), 1)
         cv.line(img, center, newPoint, color=(0,255/2,0))
 
@@ -86,7 +84,7 @@ def plotReprojection(points, cameraResolution, f, deltaE, rangeMeter, fScale=1./
     deltaE *= meter
     points *= meter
     points = [toValidPixel(p) for p in points]
-    doTheStuff(img, f, fScale, deltaE, center, points, calcErrPoint)
+    plot2DReprojection(img, f, fScale, deltaE, center, points, calcErrPoint)
 
     img = cv.flip(img, 0)
     return img
@@ -106,6 +104,10 @@ def calcPoseReprojectionRMSEThreshold(translationVec, rotationVec, camera, featu
         maxReprojErrX = maxReprojectionError((point3D[0], point3D[2]), camera.cameraMatrix[0, 0], deltaE=featureModel.uncertainty)
         maxReprojErrY = maxReprojectionError((point3D[1], point3D[2]), camera.cameraMatrix[1, 1], deltaE=featureModel.uncertainty)
         reprErrs.append([maxReprojErrX, maxReprojErrY])
+
+    reprErrs = np.array(reprErrs)
+    maxRMSE = np.sqrt(np.sum(reprErrs**2)/np.product(reprErrs.shape))
+
     if showImg:
         rangeMeter = featureModel.maxRad*40 # cover 40*max rad of the feature model
         img = plotReprojection(np.array(pointsX), 
@@ -141,18 +143,20 @@ def calcPoseReprojectionRMSEThreshold(translationVec, rotationVec, camera, featu
         img = cv.flip(img, 0)
         cv.imshow("reprojection", img)
         cv.waitKey(1)
-        
-
-    reprErrs = np.array(reprErrs)
-    maxRMSE = np.sqrt(np.sum(reprErrs**2)/np.product(reprErrs.shape))
 
     return maxRMSE
 
 if __name__ == "__main__":
-    cameraResolution = (1280, 720)
+    cameraResolution = (720, 1280)
     fx = 1406
     fy = 1411
-    deltaE = 0.0012
-    points = np.array([(0, .15)])
-    plotReprojection(points, cameraResolution, fx, deltaE, rangeMeter=5, fScale=1/30)
+    deltaE = 0.008545
+    points = np.array([(0, 6), (2, 6), (-2, 6)])
+
+    for p in points:
+        reprErr = maxReprojectionError(p, fx, deltaE)
+        print("Reprojection error: {}".format(reprErr))
+
+    img = plotReprojection(points, cameraResolution, fx, deltaE, rangeMeter=10, fScale=1./20)
+    cv.imshow("reprojection", img)
     cv.waitKey(0)