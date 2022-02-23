2b57c68eb6526c6ffa4ea70f6a9b91d48e76c50f
Joar <joared@kth.se>
2022-02-23 15:02:38 +0100

    inverse_transform_node now displays translation, orientation and errors for both

--- nodes/inverse_transform_node.py
+++ nodes/inverse_transform_node.py
@@ -21,6 +21,150 @@ class InverseTransformNode:
 
         self.listener = tf.TransformListener()
 
+    def plotTranslation(self, subPlotRows, subPlotCols, startIdx):
+        titles = ["X", "Y", "Z"]
+        colors = ["r", "g", "b"]
+        for i, (c, title) in enumerate(zip(colors, titles)):
+            plt.subplot(subPlotRows, subPlotCols, 2*i+1 + startIdx)
+            plt.cla()
+            plt.gca().set_ylabel(title)
+
+            plt.plot([p[i] for p in self.poses1], color=c)
+            plt.plot([p[i] for p in self.poses2], "-o", color=c)
+
+            plt.subplot(subPlotRows, subPlotCols, 2*i+2 + startIdx)
+            plt.cla()
+            if i == 0:
+                plt.title("Average error, {} samples".format(self.movingErrAverage))
+            
+            diffs = [d[i] for d in self.diffs]
+            if self.movingErrAverage > 1:
+                n = self.movingErrAverage
+                diffs = list(np.convolve(diffs, np.ones(n)/n, mode="same"))
+
+            # center error around 0
+            yAbsMax = max(diffs, key=abs)
+            marginP = 0.1
+            yAbsMax += yAbsMax*marginP
+            plt.gca().set_ylim(ymin=-yAbsMax, ymax=yAbsMax)
+
+            # zeros reference line
+            plt.plot(np.zeros(len(diffs)), "--", color="gray")
+            plt.plot(diffs, color=c)
+
+    def plotOrientation(self, subPlotRows, subPlotCols, startIdx):
+        titles = ["Roll", "Pitch", "Yaw"]
+        colors = ["r", "g", "b"]
+        for i, (c, title) in enumerate(zip(colors, titles)):
+            plt.subplot(subPlotRows, subPlotCols, 2*i+1 + startIdx)
+            plt.cla()
+            plt.gca().set_ylabel(title)
+
+            plt.plot([p[3+i] for p in self.poses1], color=c)
+            plt.plot([p[3+i] for p in self.poses2], "-o", color=c)
+
+            plt.subplot(subPlotRows, subPlotCols, 2*i+2 + startIdx)
+            plt.cla()
+            if i == 0:
+                plt.title("Average error, {} samples".format(self.movingErrAverage))
+            
+            diffs = [d[3+i] for d in self.diffs]
+            if self.movingErrAverage > 1:
+                n = self.movingErrAverage
+                diffs = list(np.convolve(diffs, np.ones(n)/n, mode="same"))
+
+            # center error around 0
+            yAbsMax = max(diffs, key=abs)
+            marginP = 0.1
+            yAbsMax += yAbsMax*marginP
+            plt.gca().set_ylim(ymin=-yAbsMax, ymax=yAbsMax)
+
+            # zeros reference line
+            plt.plot(np.zeros(len(diffs)), "--", color="gray")
+            plt.plot(diffs, color=c)
+
+    def plotPose2(self):
+        titles = ["X", "Y", "Z"]
+        colors = ["r", "g", "b"]
+        subPlotRows = 6
+        subPlotCols = 2
+        for i, (c, title) in enumerate(zip(colors, titles)):
+            plt.subplot(subPlotRows, subPlotCols, 2*i+1)
+            plt.cla()
+            plt.gca().set_ylabel(title)
+
+            plt.plot([p[i] for p in self.poses1], color=c)
+            plt.plot([p[i] for p in self.poses2], "-o", color=c)
+
+            plt.subplot(subPlotRows, subPlotCols, 2*i+2)
+            plt.cla()
+            if i == 0:
+                plt.title("Average error, {} samples".format(self.movingErrAverage))
+            
+            diffs = [d[i] for d in self.diffs]
+            if self.movingErrAverage > 1:
+                n = self.movingErrAverage
+                diffs = list(np.convolve(diffs, np.ones(n)/n, mode="same"))
+
+            # center error around 0
+            yAbsMax = max(diffs, key=abs)
+            marginP = 0.1
+            yAbsMax += yAbsMax*marginP
+            plt.gca().set_ylim(ymin=-yAbsMax, ymax=yAbsMax)
+
+            # zeros reference line
+            plt.plot(np.zeros(len(diffs)), "--", color="gray")
+            plt.plot(diffs, color=c)
+
+        # plot orientation
+        titles = ["Roll", "Pitch", "Yaw"]
+        colors = ["r", "g", "b"]
+        for i, (c, title) in enumerate(zip(colors, titles)):
+            plt.subplot(subPlotRows, subPlotCols, 2*i+1 + subPlotRows)
+            plt.cla()
+            plt.gca().set_ylabel(title)
+
+            plt.plot([p[3+i] for p in self.poses1], color=c)
+            plt.plot([p[3+i] for p in self.poses2], "-o", color=c)
+
+            plt.subplot(subPlotRows, subPlotCols, 2*i+2 + subPlotRows)
+            plt.cla()
+            
+            diffs = [d[3+i] for d in self.diffs]
+            if self.movingErrAverage > 1:
+                n = self.movingErrAverage
+                diffs = list(np.convolve(diffs, np.ones(n)/n, mode="same"))
+
+            # center error around 0
+            yAbsMax = max(diffs, key=abs)
+            marginP = 0.1
+            yAbsMax += yAbsMax*marginP
+            plt.gca().set_ylim(ymin=-yAbsMax, ymax=yAbsMax)
+
+            # zeros reference line
+            plt.plot(np.zeros(len(diffs)), "--", color="gray")
+            plt.plot(diffs, color=c)
+
+    def plotPose(self):
+        colors = ["r", "g", "b"]
+
+        # plot translation
+        plt.subplot(2, 2, 1)
+        plt.cla()
+        plt.gca().set_ylabel("Translation X, Y, Z")
+        for i, c in enumerate(colors):
+            plt.plot([p[i] for p in self.poses1], color=c)
+            plt.plot([p[i] for p in self.poses2], "-o", color=c)
+            
+        # plot orientation
+        plt.subplot(2, 2, 3)
+        plt.cla()
+        plt.gca().set_ylabel("Orientation X, Y, Z")
+        for i, c in enumerate(colors):
+            plt.plot([p[3+i] for p in self.poses1], color=c)
+            plt.plot([p[3+i] for p in self.poses2], "-o", color=c)
+
+
     def transToPose(self, transl, rot):
         euler = R.from_quat(rot).as_euler("YXZ")
         pose = list(transl) + [euler[2], euler[1], euler[0]] # [x, y, z, roll, pitch yaw]
@@ -66,39 +210,13 @@ class InverseTransformNode:
                 self.poses1 = self.poses1[-self.bufferLength:]
                 self.poses2 = self.poses2[-self.bufferLength:]
                 self.diffs = self.diffs[-self.bufferLength:]
-                
-                titles = ["X", "Y", "Z"]
-                colors = ["r", "g", "b"]
-                for i, (c, title) in enumerate(zip(colors, titles)):
-                    plt.subplot(3, 2, 2*i+1)
-                    plt.cla()
-                    plt.gca().set_ylabel(title)
-
-                    plt.plot([p[i] for p in self.poses1], color=c)
-                    plt.plot([p[i] for p in self.poses2], "-o", color=c)
-
-                    plt.subplot(3, 2, 2*i+2)
-                    plt.cla()
-                    if i == 0:
-                        plt.title("Average error, {} samples".format(self.movingErrAverage))
-                    
-                    diffs = [d[i] for d in self.diffs]
-                    if self.movingErrAverage > 1:
-                        n = self.movingErrAverage
-                        diffs = list(np.convolve(diffs, np.ones(n)/n, mode="same"))
-
-                    # center error around 0
-                    yAbsMax = max(diffs, key=abs)
-                    marginP = 0.1
-                    yAbsMax += yAbsMax*marginP
-                    plt.gca().set_ylim(ymin=-yAbsMax, ymax=yAbsMax)
-
-                    # zeros reference line
-                    plt.plot(np.zeros(len(diffs)), "--", color="gray")
-                    plt.plot(diffs, color=c)
-
+         
+                self.plotPose2()
                 plt.pause(0.001)
+
             rate.sleep()
+
+        
 
 if __name__ == '__main__':
     rospy.init_node("inverse_transform_node")
