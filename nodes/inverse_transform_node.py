889807e86605434effd644a9c8a6fe60b3dbd06b
Joar <joared@kth.se>
2022-02-24 02:26:36 +0100

    Added 3D plot to inverse_transform_node.py

--- nodes/inverse_transform_node.py
+++ nodes/inverse_transform_node.py
@@ -3,7 +3,10 @@ import rospy
 import tf
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D # for python 2.7
 from scipy.spatial.transform import Rotation as R
+
+from lolo_simulation.coordinate_system import CoordinateSystemArtist
 
 class InverseTransformNode:
     def __init__(self, trans1Name, trans2Name, referenceFrame=None, bufferLength=500, movingErrAverage=1):
@@ -158,25 +161,44 @@ class InverseTransformNode:
 
             plt.plot(diffs[:, 3+i], color=c)
 
-    def plotPose(self):
-        colors = ["r", "g", "b"]
+    def plot3D(self):
+        if not self.poses1 or not self.poses2:
+            return
 
-        # plot translation
-        plt.subplot(2, 2, 1)
-        plt.cla()
-        plt.gca().set_ylabel("Translation X, Y, Z")
-        for i, c in enumerate(colors):
-            plt.plot([p[i] for p in self.poses1], color=c)
-            plt.plot([p[i] for p in self.poses2], "-o", color=c)
-            
-        # plot orientation
-        plt.subplot(2, 2, 3)
-        plt.cla()
-        plt.gca().set_ylabel("Orientation X, Y, Z")
-        for i, c in enumerate(colors):
-            plt.plot([p[3+i] for p in self.poses1], color=c)
-            plt.plot([p[3+i] for p in self.poses2], "-o", color=c)
+        poses1 = np.array(self.poses1)
+        poses2 = np.array(self.poses2)
 
+        maxX = max(np.max(poses1[:, 0]), np.max(poses2[:, 0]))
+        minX = min(np.min(poses1[:, 0]), np.min(poses2[:, 0]))
+        absMaxX = max(abs(maxX), abs(minX))
+
+        maxY = max(np.max(poses1[:, 1]), np.max(poses2[:, 1]))
+        minY = min(np.min(poses1[:, 1]), np.min(poses2[:, 1]))
+        absMaxY = max(abs(maxY), abs(minY))
+
+        absMaxXY = max(absMaxX, absMaxY)
+
+        maxZ = max(np.max(poses1[:, 2]), np.max(poses2[:, 2]))
+        minZ = min(np.min(poses1[:, 2]), np.min(poses2[:, 2]))
+
+        absMaxXYZ = max(absMaxXY, maxZ/2)
+        self.ax.cla()
+
+        self.ax.set_xlim(-absMaxXYZ, absMaxXYZ)
+        self.ax.set_ylim(-absMaxXYZ, absMaxXYZ)
+        self.ax.set_zlim(0, absMaxXYZ*2)
+        
+        self.ax.plot(poses2[:, 0], poses2[:, 1], poses2[:, 2], "-o", markersize=1.6, color="r")
+        self.ax.plot(poses1[:, 0], poses1[:, 1], poses1[:, 2], linewidth=1, color="g")
+
+        csCamera = CoordinateSystemArtist()
+        csCamera.draw(self.ax)
+
+        csLastPose = CoordinateSystemArtist(scale=.4)
+        csLastPose.cs.translation = self.poses2[-1][:3]
+        ax, ay, az = self.poses2[-1][3:]
+        csLastPose.cs.rotation = R.from_euler("YXZ", (ay, ax, az)).as_dcm()
+        csLastPose.draw(self.ax)
 
     def transToPose(self, transl, rot):
         euler = R.from_quat(rot).as_euler("YXZ")
@@ -184,8 +206,12 @@ class InverseTransformNode:
         return np.array(pose)
 
     def run(self):
-        rate = rospy.Rate(100)
-        plt.figure()
+        rate = rospy.Rate(10)
+        #rospy.Timer(rospy.Duration(1.0/2.0), lambda event: self.plot3D())
+        self.fig = plt.gcf()
+        self.fig.add_subplot(projection="3d")
+        self.ax = Axes3D(self.fig)
+
         while not rospy.is_shutdown():
             timeStamp = rospy.Time(0)
             pose1 = None
@@ -224,9 +250,11 @@ class InverseTransformNode:
                 self.poses2 = self.poses2[-self.bufferLength-self.movingErrAverage+1:]
                 self.diffs = self.diffs[-self.bufferLength-self.movingErrAverage+1:]
          
-                self.plotPose2()
-                plt.pause(0.001)
-
+                #self.plotPose2()
+                self.plot3D()
+                plt.pause(0.000001)
+            
+            
             rate.sleep()
 
         
@@ -236,7 +264,7 @@ if __name__ == '__main__':
     invTransNode = InverseTransformNode("docking_station/feature_model_link", 
                                         "docking_station/feature_model_estimated_link", 
                                         referenceFrame="lolo_camera_link", 
-                                        bufferLength=50,
-                                        movingErrAverage=5)
+                                        bufferLength=500,
+                                        movingErrAverage=1)
 
     invTransNode.run()
