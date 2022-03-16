Changed pose average estimator node to calculate covariance based on fixed axis convention, converted projected points to integers in pose estimation node (should be distinct pixel values) and added a test script for analyzing the object pose image jacobian

nodes/pose_average_estimator_node.py
@@ -10,6 +10,7 @@ import glob
 import time
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.spatial.transform import Rotation as R
 
 from lolo_perception.perception_ros_utils import vectorToPose, poseToVector
 
@@ -58,12 +59,15 @@ class PoseAverageEstimatorNode:
     def poseCallback(self, pose):
         self.frameID = pose.header.frame_id
         translationVector, rotationVector = poseToVector(pose)
-        self.estimator.add(translationVector, rotationVector)
+        fixedEuler = np.array(R.from_rotvec(rotationVector).as_euler("xyz"))
+        self.estimator.add(translationVector, fixedEuler)
 
     def calcPose(self):
         poseAvg = self.estimator.calcAverage()
         poseCov = self.estimator.calcCovariance()
-        return vectorToPose(self.frameID, poseAvg[:3], poseAvg[3:], poseCov)
+
+        rotVec = R.from_euler("xyz", poseAvg[3:]).as_rotvec()
+        return vectorToPose(self.frameID, poseAvg[:3], rotVec, poseCov)
 
     def run(self):
         rate = rospy.Rate(30)

nodes/pose_estimation_simulation_node.py
@@ -69,7 +69,7 @@ class PoseSimulation:
         trueRotation = np.eye(3, dtype=np.float32)
         ax, ay, az = 0, 0, 0 # euler angles
         featureIdx = -1
-        rate = rospy.Rate(20)
+        rate = rospy.Rate(5)
         while not rospy.is_shutdown():
             
             linInc = 0.1
@@ -125,7 +125,7 @@ class PoseSimulation:
             projPointsNoised = biasedProjPoints.copy()
             projPointsNoised[:, 0] += np.random.normal(0, sigmaX, biasedProjPoints[:, 0].shape)
             projPointsNoised[:, 1] += np.random.normal(0, sigmaY, biasedProjPoints[:, 1].shape)
-
+            projPointsNoised = projPointsNoised.round().astype(np.int32)
             # estimate pose (translation and rotation in camera frame)
             estTranslationVec = trueTrans.copy()
             estRotationVec = trueRotation.copy()
@@ -136,8 +136,10 @@ class PoseSimulation:
             # TODO: use something else than max intensity (255)?
             lightSourcesNoised = [ LightSource( np.array([[[ p[0], p[1] ]]], dtype=np.int32), intensity=255 ) for p in projPointsNoised]
             dsPoseNoised = self.poseEstimator.estimatePose(lightSourcesNoised, 
-                                                      estTranslationVec,
-                                                      estRotationVec)
+                                                           estTranslationVec,
+                                                           estRotationVec)
+                                                           #np.array([0., 0, 1]),
+                                                           #np.array([0., 0, 0])
             
             # calculates covariance based on max reprojection rmse from feature uncertainty
             dsPoseNoised.calcCovariance()
@@ -180,7 +182,7 @@ class PoseSimulation:
             plotPoints(imgTemp, projPointsNoised, color=(255,0,255))
             
             self.imagePublisher.publish(self.cvBridge.cv2_to_imgmsg(imgTemp))
-
+            print("{} < {}".format(dsPoseNoised.rmse, dsPoseNoised.rmseMax))
             cv.imshow("feature model modification", imgTemp)
             rate.sleep()
 
@@ -195,6 +197,9 @@ if __name__ =="__main__":
     featureModelYaml = rospy.get_param("~feature_model_yaml")
     featureModelYamlPath = os.path.join(rospkg.RosPack().get_path("lolo_perception"), "feature_models/{}".format(featureModelYaml))
     featureModel = FeatureModel.fromYaml(featureModelYamlPath)
+
+    print("Placement unc:", featureModel.placementUncertainty)    
+    print("Detection tolerance:", featureModel.detectionTolerance)
 
     cameraYaml = rospy.get_param("~camera_yaml")
     cameraYamlPath = os.path.join(rospkg.RosPack().get_path("lolo_perception"), "camera_calibration_data/{}".format(cameraYaml))